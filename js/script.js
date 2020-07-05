65ca72d77da0fd2566e77ff43be3d7c6e34e093f
unknown <ex@gmail.com>
2020-07-05 15:25:41 +0300

    first commit '

---
+++ js/script.js
@@ -0,0 +1,8 @@
+"use strict";
+//alert('hello');
+//const result= confirm ('are you here');
+//console.log(result);
+//const cat = 'tous';
+//console.log(`hui${cat}/5` );
+const user = 'ivan';
+alert(`пртвет, ${user}`); 
